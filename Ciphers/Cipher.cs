c7bbfea4dfa066b0a3cf0a08aa3e42bd5735308c
Kuba Szostko <kubaszostko@gmail.com>
2021-02-23 14:54:17 +0100

    created cipher interface

---
+++ Ciphers/Cipher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCiphers.Ciphers
+{
+    interface Cipher
+    {
+        string Encrypt(string s);
+        string Decrypt(string s);
+    }
+}
