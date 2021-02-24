a59602f855b11bc4f5b916ee452ccffad60d1ee5
Filip Sasinowski <filip.sasinowski@gmail.com>
2021-02-25 00:37:09 +0100

    Working RailFence testing UI.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using WPFCiphers.Ciphers;
 namespace WPFCiphers
 {
     /// <summary>
@@ -25,5 +25,74 @@ namespace WPFCiphers
         {
             InitializeComponent();
         }
+
+
+        // cipherButtonPressed fields...
+        string currentAlgorithm = "none";
+        RailFence rf;
+        private void cipherButtonPressed(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string buttonName = ((Button)sender).Name;
+            mTextBox.Text = " ";
+            keyTextBox.Text = " ";
+            if (buttonName == currentAlgorithm)
+            {
+                sideBarGrid.Visibility = Visibility.Hidden;
+                currentAlgorithm = "none";
+            }
+            else
+            {
+                sideBarGrid.Visibility = Visibility.Visible;
+                currentAlgorithm = buttonName;
+                algoNameLabel.Content = buttonName;
+
+             
+                switch (currentAlgorithm)
+                {
+                    case "RAIL_FENCE":
+                        mLabel.Content = "M";
+                        keyLabel.Content = "n";
+                        break;
+                    case "COLUMNAR_TRANSP":
+                        mLabel.Content = "M";
+                        keyLabel.Content = "key";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void doTheMagic(object sender, RoutedEventArgs e)
+        {
+            switch (currentAlgorithm)
+            {
+                case "RAIL_FENCE":
+                    if (validateRailfenceFields())
+                    {
+                        int i = int.Parse(keyTextBox.Text.ToString());
+                        rf = new RailFence(i);
+                        string encrypted = rf.Encrypt(mTextBox.Text.ToString());
+                        string decrypted = rf.Encrypt(encrypted);
+                        encryptedLabel.Content = encrypted;
+                        decryptedLabel.Content = decrypted;
+                    }
+                    break;
+                case "COLUMNAR_TRANSP":
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+        private bool validateRailfenceFields()
+        {
+            int i;
+            if (int.TryParse(keyTextBox.Text.ToString(), out i))
+                return true;
+            return false;
+        }
     }
+
+   
 }
