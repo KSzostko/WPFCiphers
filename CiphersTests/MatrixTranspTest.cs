0e09dc38d55fe4b27af64efe21b06a06bd967f23
Kuba Szostko <kubaszostko@gmail.com>
2021-03-01 16:36:56 +0100

    added tests for words with same letters for MatrixTransp

--- CiphersTests/MatrixTranspTest.cs
+++ CiphersTests/MatrixTranspTest.cs
@@ -27,5 +27,29 @@ namespace CiphersTests
             string decrypted = tp.Decrypt(input);
             Assert.AreEqual(input, decrypted);
         }
+        
+        [TestMethod]
+        public void ReturnsLettersSpiltWithSpacesForVeryLongKeyWithSameLettersEncryption()
+        {
+            string input = "kot ma ale";
+            string expected = "k o t m a a l e";
+
+            MatrixTransp tp = new MatrixTransp("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            string encrypted = tp.Encrypt(input);
+            Assert.AreEqual(expected, encrypted);
+        }
+        
+        [TestMethod]
+        public void ReturnsLettersWithoutSpacesForVeryLongKeyWithSameLettersDecryption()
+        {
+            string input = "k o t m a a l e";
+            string expected = "kotmaale";
+
+            MatrixTransp tp = new MatrixTransp("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            string decrypted = tp.Decrypt(input);
+            Assert.AreEqual(expected, decrypted);
+        }
     }
 }
