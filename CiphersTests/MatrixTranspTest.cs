795732699934e4dc533bc4069f08b1c88a565d6e
Kuba Szostko <kubaszostko@gmail.com>
2021-03-01 16:28:52 +0100

    added tests for MatrixTransp with 1 letter key

---
+++ CiphersTests/MatrixTranspTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFCiphers.Ciphers;
+
+namespace CiphersTests
+{
+    [TestClass]
+    public class MatrixTranspTest
+    {
+        [TestMethod]
+        public void ReturnsUnchangedWordFor1LetterKeyEncryption()
+        {
+            string input = "Dsadajkaajddjh";
+
+            MatrixTransp tp = new MatrixTransp("a");
+
+            string encrypted = tp.Encrypt(input);
+            Assert.AreEqual(input, encrypted);
+        }
+        
+        [TestMethod]
+        public void ReturnsUnchangedWordFor1LetterKeyDecryption()
+        {
+            string input = "Dsadajkaajddjh";
+
+            MatrixTransp tp = new MatrixTransp("a");
+
+            string decrypted = tp.Decrypt(input);
+            Assert.AreEqual(input, decrypted);
+        }
+    }
+}
