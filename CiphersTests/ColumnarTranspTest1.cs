a44707c5c14bdf4501cb249a39cfffb57de5879b
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2021-03-01 12:24:15 +0100

    dodałem proste testy do ColumnarTraspTest1

--- CiphersTests/ColumnarTranspTest1.cs
+++ CiphersTests/ColumnarTranspTest1.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+//
+using WPFCiphers.Ciphers;
 
 namespace CiphersTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ColumnarTranspTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void EncryptTest()
         {
+            int[] key = new int[] { 3, 1, 4, 2 };
+            string input = "CRYPTOGRAPHYOSA";
+            string expected = "YCPRGTROHAYPAOS";
+
+            ColumnarTransposition ct = new ColumnarTransposition(key);
+            string output = ct.Encrypt(input);
+
+            Assert.AreEqual(output, expected);
+
+        }
+
+        [TestMethod]
+        public void DecryptTest()
+        {
+            int[] key = new int[] { 3, 1, 4, 2 };
+            string input = "YCPRGTROHAYPAOS"; 
+            string expected = "CRYPTOGRAPHYOSA";
+
+            ColumnarTransposition ct = new ColumnarTransposition(key);
+            string output = ct.Decrypt(input);
+
+            Assert.AreEqual(output, expected);
+
         }
     }
 }
