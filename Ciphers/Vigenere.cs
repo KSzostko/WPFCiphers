8389cbfa2a365d42b3dce8a61873385b9417409c
Kuba Szostko <kubaszostko@gmail.com>
2021-03-07 11:24:43 +0100

    created Vigenere class

---
+++ Ciphers/Vigenere.cs
@@ -0,0 +1,23 @@
+﻿namespace WPFCiphers.Ciphers
+{
+    public class Vigenere : Cipher
+    {
+        private static readonly int ALPHABET_LETTERS_COUNT = 26;
+        
+        public string Key { get; set; }
+
+        public Vigenere(string key)
+        {
+            Key = key.ToUpper();
+        }
+        public string Encrypt(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Decrypt(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
