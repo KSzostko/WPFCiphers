d296da8d7a19d7e7a5a57303ba7f4cd347d252c6
Kuba Szostko <kubaszostko@gmail.com>
2021-03-07 11:35:16 +0100

    added tests for Vinegere class

---
+++ CiphersTests/VinegereTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFCiphers.Ciphers;
+
+namespace CiphersTests
+{
+    [TestClass]
+    public class VinegereTest
+    {
+        [TestMethod]
+        public void ReturnsEncryptedWordForKeyMatchingWordLength()
+        {
+            string input = "CRYPTOGRAPHY";
+            string expected = "DICPDPXVAZIP";
+
+            Vigenere vigenere = new Vigenere("BREAKBREAKBR");
+
+            string encrypted = vigenere.Encrypt(input);
+            Assert.AreEqual(expected, encrypted);
+        }
+        
+        [TestMethod]
+        public void ReturnsDecryptedWordForKeyMatchingWordLength()
+        {
+            string input = "DICPDPXVAZIP";
+            string expected = "CRYPTOGRAPHY";
+
+            Vigenere vigenere = new Vigenere("BREAKBREAKBR");
+
+            string decrypted = vigenere.Decrypt(input);
+            Assert.AreEqual(expected, decrypted);
+        }
+
+        [TestMethod]
+        public void ReturnsEncryptedWordForKeyShorterThanWord()
+        {
+            string input = "CRYPTOGRAPHY";
+            string expected = "DICPDPXVAZIP";
+
+            Vigenere vigenere = new Vigenere("BREAK");
+
+            string encrypted = vigenere.Encrypt(input);
+            Assert.AreEqual(expected, encrypted);
+        }
+        
+        [TestMethod]
+        public void ReturnsDecryptedWordForKeyShorterThanWord()
+        {
+            string input = "DICPDPXVAZIP";
+            string expected = "CRYPTOGRAPHY";
+
+            Vigenere vigenere = new Vigenere("BREAK");
+
+            string decrypted = vigenere.Decrypt(input);
+            Assert.AreEqual(expected, decrypted);
+        }
+    }
+}
