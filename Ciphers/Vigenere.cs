6586bcb07df6f3627a256bf1e0536ee44fce31c5
Kuba Szostko <kubaszostko@gmail.com>
2021-03-07 11:26:19 +0100

    created method for adjusting key to given word in vinegere class

--- Ciphers/Vigenere.cs
+++ Ciphers/Vigenere.cs
@@ -12,12 +12,29 @@
         }
         public string Encrypt(string s)
         {
+            string adjustedKey = AdjustKeyToWord(s);
             throw new System.NotImplementedException();
         }
 
         public string Decrypt(string s)
         {
             throw new System.NotImplementedException();
+        }
+        
+        private string AdjustKeyToWord(string s)
+        {
+            if (Key.Length >= s.Length) return Key;
+            
+            string newKey = Key;
+            int current = 0;
+
+            while (newKey.Length < s.Length)
+            {
+                newKey += Key[current];
+                current = current == Key.Length - 1 ? 0 : current + 1;
+            }
+
+            return newKey;
         }
     }
 }
