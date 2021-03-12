53a4963a024428c7b173aef544a7d70f340d64b7
Kuba Szostko <kubaszostko@gmail.com>
2021-03-12 12:04:34 +0100

    added tests with small letters for Vinegere, all tests are failing

--- CiphersTests/VinegereTest.cs
+++ CiphersTests/VinegereTest.cs
@@ -53,5 +53,53 @@ namespace CiphersTests
             string decrypted = vigenere.Decrypt(input);
             Assert.AreEqual(expected, decrypted);
         }
+        
+        [TestMethod]
+        public void ReturnsEncryptedWordForKeyMatchingWordLengthWithSmallLetters()
+        {
+            string input = "cryptography";
+            string expected = "DICPDPXVAZIP";
+
+            Vigenere vigenere = new Vigenere("breakbreakbr");
+
+            string encrypted = vigenere.Encrypt(input);
+            Assert.AreEqual(expected, encrypted);
+        }
+        
+        [TestMethod]
+        public void ReturnsDecryptedWordForKeyMatchingWordLengthWithSmallLetters()
+        {
+            string input = "dicpdpxvazip";
+            string expected = "CRYPTOGRAPHY";
+
+            Vigenere vigenere = new Vigenere("breakbreakbr");
+
+            string decrypted = vigenere.Decrypt(input);
+            Assert.AreEqual(expected, decrypted);
+        }
+
+        [TestMethod]
+        public void ReturnsEncryptedWordForKeyShorterThanWordWithSmallLetters()
+        {
+            string input = "cryptography";
+            string expected = "DICPDPXVAZIP";
+
+            Vigenere vigenere = new Vigenere("break");
+
+            string encrypted = vigenere.Encrypt(input);
+            Assert.AreEqual(expected, encrypted);
+        }
+        
+        [TestMethod]
+        public void ReturnsDecryptedWordForKeyShorterThanWordWithSmallLetters()
+        {
+            string input = "dicpdpxvazip";
+            string expected = "CRYPTOGRAPHY";
+
+            Vigenere vigenere = new Vigenere("break");
+
+            string decrypted = vigenere.Decrypt(input);
+            Assert.AreEqual(expected, decrypted);
+        }
     }
 }
