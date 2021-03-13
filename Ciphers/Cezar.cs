16ad559be0731b58279a73e87f276b2a29916a99
Filip Sasinowski <filip.sasinowski@gmail.com>
2021-03-13 22:50:54 +0100

    Final version of cezar cipher.

--- Ciphers/Cezar.cs
+++ Ciphers/Cezar.cs
@@ -7,37 +7,41 @@
         {
             this.cipherKey = key;
         }
-              public static char cipher(char ch, int key) {  
-            if (!char.IsLetter(ch)) {  
-  
-                return ch;  
-            }  
-  
-            char d = char.IsUpper(ch) ? 'A' : 'a';  
-            return (char)((((ch + key) - d) % 26) + d);  
-  
-  
-        }  
-  
-  
-        public string Encrypt(string input) {
+        public static char cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+
+                return ch;
+            }
+
+            char d = char.IsUpper(ch) ? 'A' : 'a';
+            return (char)((((ch + key) - d) % 26) + d);
+
+
+        }
+
+
+        public string Encrypt(string input)
+        {
             int key = cipherKey;
-            string output = string.Empty;  
-  
-            foreach(char ch in input)  
-            output += cipher(ch, key);  
-  
-            return output;  
-        }  
-  
-        public string Decrypt(string input) {
-            int key =  26 - cipherKey;
             string output = string.Empty;
 
             foreach (char ch in input)
                 output += cipher(ch, key);
 
             return output;
-        } 
+        }
+
+        public string Decrypt(string input)
+        {
+            int key = 26 - cipherKey;
+            string output = string.Empty;
+
+            foreach (char ch in input)
+                output += cipher(ch, key);
+
+            return output;
+        }
     }
 }
