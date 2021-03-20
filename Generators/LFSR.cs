c8139594d4a3d9709001eac996177e39d7874e52
Kuba Szostko <kubaszostko@gmail.com>
2021-03-20 17:10:00 +0100

    feat: create lfsr class

---
+++ Generators/LFSR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WPFCiphers.Generators
+{
+    public class LFSR : RandomGenerator
+    {
+        public void StartGenerator(int[] powers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GenerateSequence(int[] powers)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StopGenerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<bool> GetSequence()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
