42b1584a53617be097dc491e3c24071760bab82b
Jakub Szkiłądź <jakub.szkiladz@wp.pl>
2021-03-27 21:47:36 +0100

    testy do nowego SSC

---
+++ CiphersTests/SynchronousStreamCipherTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPFCiphers.Ciphers;
+
+namespace CiphersTests
+{
+    [TestClass]
+    public class SynchronousStreamCipherTest
+    {
+        [TestMethod]
+        public void EncryptTest()
+        {
+            List<bool> key = new List<bool> { true, false, false, false, true, false, true, true, true };
+            string input = "test_ssc_encrypt.txt";
+
+            SynchronousStreamCipher ssc = new SynchronousStreamCipher(key);
+            BitArray output = ssc.Encrypt(input);
+
+            Assert.IsNotNull(output);
+        }
+
+        [TestMethod]
+        public void DecryptTest()
+        {
+            List<bool> key = new List<bool> { true, false, false, false, true, false, true, true, true };
+            string input = "test_ssc_encrypt.txt";
+
+            SynchronousStreamCipher ssc = new SynchronousStreamCipher(key);
+            BitArray output = ssc.Decrypt(input);
+
+            Assert.IsNotNull(output);
+        }
+    }
+}
