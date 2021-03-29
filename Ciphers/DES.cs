a0b500b9a0360011c098be4ee20f092013c32613
Kuba Szostko <kubaszostko@gmail.com>
2021-03-29 10:29:07 +0200

    feat: add DES cipher class

---
+++ Ciphers/DES.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace WPFCiphers.Ciphers
+{
+    public class DES : Cipher
+    {
+        public string Key { get; set; }
+
+        public DES(string key)
+        {
+            Key = key;
+        }
+
+        public string Encrypt(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Decrypt(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
