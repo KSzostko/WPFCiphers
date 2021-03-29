507cd6ffd9437d9b6813f4405f261bc80b0e6777
Kuba Szostko <kubaszostko@gmail.com>
2021-03-29 10:41:38 +0200

    feat(des): append bits for input to divide it by 64

--- Ciphers/DES.cs
+++ Ciphers/DES.cs
@@ -13,12 +13,28 @@ namespace WPFCiphers.Ciphers
 
         public string Encrypt(string s)
         {
+            string adjustedInput = AppendBits(s);
             throw new System.NotImplementedException();
         }
 
         public string Decrypt(string s)
         {
             throw new System.NotImplementedException();
+        }
+        
+        private string AppendBits(string s)
+        {
+            if (s.Length % 64 == 0) return s;
+
+            StringBuilder builder = new StringBuilder(s);
+            
+            builder.Append('1');
+            while (builder.Length % 64 != 0)
+            {
+                builder.Append('0');
+            }
+
+            return builder.ToString();
         }
     }
 }
