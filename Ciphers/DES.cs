dd38ae9247719094707b68dec4163c25393a9508
Kuba Szostko <kubaszostko@gmail.com>
2021-03-29 14:20:00 +0200

    feat(des): add method for performing permutation with PS on key

--- Ciphers/DES.cs
+++ Ciphers/DES.cs
@@ -15,6 +15,16 @@ namespace WPFCiphers.Ciphers
             {61, 53, 45, 37, 29, 21, 13, 5},
             {63, 55, 47, 39, 31, 23, 15, 7}
         };
+        private static readonly int[,] PermutedChoice = {
+            {57, 49, 41, 33, 25, 17, 9},
+            {1, 58, 50, 42, 34, 26, 18},
+            {10, 2, 59, 51, 43, 35, 27},
+            {19, 11, 3, 60, 52, 44, 36},
+            {63, 55, 47, 39, 31, 23, 15},
+            {7, 62, 54, 46, 38, 30, 22},
+            {14, 6, 61, 53, 45, 37, 29},
+            {21, 13, 5, 28, 20, 12, 4}
+        };
         
         public string Key { get; set; }
 
@@ -27,6 +37,11 @@ namespace WPFCiphers.Ciphers
         {
             string input = AppendBits(s);
             input = PerformInitialPermutation(input);
+            
+            // step 3 not implemented for now
+
+            string reducedKey = PerformKeyPermutation();
+
             throw new System.NotImplementedException();
         }
 
@@ -62,6 +77,18 @@ namespace WPFCiphers.Ciphers
                     int inputIndex = pos - 1 + currentBlock * BlockSize;
                     builder.Append(s[inputIndex]);
                 }
+            }
+
+            return builder.ToString();
+        }
+        
+        private string PerformKeyPermutation()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (int pos in PermutedChoice)
+            {
+                builder.Append(Key[pos - 1]);
             }
 
             return builder.ToString();
